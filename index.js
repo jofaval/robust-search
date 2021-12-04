8b032c046e0c4cc7d0c20016e01433fa563c8af0
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-04 18:26:08 +0100

    Fixed String prototype implementations

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const { almost, notQuite } = require("./src/almost");
-const { equal, different } = require("./src/equal");
+const { almost , notQuite          } = require("./src/almost");
+const { equal  , different         } = require("./src/equal");
 const { isMatch, search, searchNot } = require("./src/search");
 
 // They get implemented as "primitive" String methods
-String.prototype.equal    = (string) => equal(this, string);
-String.prototype.diff     = (string) => different(this, string);
-String.prototype.almost   = (string) => almost(this, string);
-String.prototype.notQuite = (string) => notQuite(this, string);
-String.prototype.isMatch  = (string) => isMatch(this, string);
+String.prototype.equal    = function (compare) { return equal    (this, compare); }
+String.prototype.diff     = function (compare) { return different(this, compare); }
+String.prototype.almost   = function (compare) { return almost   (this, compare); }
+String.prototype.notQuite = function (compare) { return notQuite (this, compare); }
+String.prototype.isMatch  = function (compare) { return isMatch  (this, compare); }
 
-exports.equal = equal;
+exports.equal     = equal;
 exports.different = different;
-exports.almost = almost;
-exports.notQuite = notQuite;
-exports.isMatch = isMatch;
-exports.search = search;
+exports.almost    = almost;
+exports.notQuite  = notQuite;
+exports.isMatch   = isMatch;
+exports.search    = search;
 exports.searchNot = searchNot;
