72e95ba92044fa17d7be50c0106e43a9ae7d1d80
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-04 18:44:35 +0100

    Add some sort of contains prototype

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { almost , notQuite          } = require("./src/almost");
 const { equal  , different         } = require("./src/equal");
 const { isMatch, search, searchNot } = require("./src/search");
+const { contains } = require("./src/contains");
 
 // They get implemented as "primitive" String methods
 String.prototype.equal    = function (compare) { return equal    (this, compare); }
@@ -8,6 +9,7 @@ String.prototype.diff     = function (compare) { return different(this, compare)
 String.prototype.almost   = function (compare) { return almost   (this, compare); }
 String.prototype.notQuite = function (compare) { return notQuite (this, compare); }
 String.prototype.isMatch  = function (compare) { return isMatch  (this, compare); }
+String.prototype.contains = function (compare) { return contains (this, compare); }
 
 exports.equal     = equal;
 exports.different = different;
@@ -16,3 +18,4 @@ exports.notQuite  = notQuite;
 exports.isMatch   = isMatch;
 exports.search    = search;
 exports.searchNot = searchNot;
+exports.contains  = contains;
---
+++ src/contains.js
@@ -0,0 +1,65 @@
+// TODO: implement, string and array, two different implementations, one calls the other?
+/**
+ * 
+ * @param {String} original The original string to compare
+ * @param {String} compare The second string to be compared
+ * @param {Object} [options] The configuration parameters
+ * @param {Boolean} [options.inOrder] Must it be in the same exact order? True by default
+ * @param {Boolean} [options.caseSensitive] Will it take into account upper/lower case difference.
+ * 
+ * @returns {Boolean} Does it contain the substring?
+ */
+const contains = (original, compare, options = {}) => {
+    if (options)
+
+    // includes is way faster
+    let matches = 0;
+
+    const originalLen = original.length;
+    const compareLen = compare.length;
+
+    // If the substring is longer than the original, it won't be found
+    if (compareLen > originalLen) return false;
+
+    if (!options?.inOrder) options?.inOrder = false;
+    if (!options?.caseSensitive) options?.caseSensitive = false;
+
+    const { inOrder, caseSensitive } = options;
+
+    // Implements case insensitive options, it's sensitive by default
+    if (!caseSensitive) {
+        original = original.toUpperCase();
+        compare = compare.toUpperCase();
+    }
+
+    // Loop through the original
+    for (let originalIndex = 0; originalIndex < originalLen; originalIndex++) {
+        const originalChar = original[originalIndex];
+        
+        // Comparison starts at the last char it checked
+        for (let compareIndex = matches; compareIndex < compareLen; compareIndex++) {
+            const compareChar = compare[compareIndex];
+            
+            // If there's a match, matches gets updated, if there isn't matches gets a reset
+            if (originalChar == compareChar) {
+                matches++;
+
+                // It'll continue to the original's next char
+                break;
+            }
+
+            // Updated matches only if it must be in the same exact order
+            if (inOrder) matches = 0;
+        }
+
+        // If there was already a full match, exit
+        if (matches == compareLen) break;
+    }
+
+    // If the matches is the compareLen, its a match
+    const contains = matches == compareLen;
+
+    return contains;
+}
+
+exports.contains = contains;
