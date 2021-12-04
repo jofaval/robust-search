5f96f1bef6161408d8cd31c5f610e56da4b6a459
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-04 17:06:24 +0100

    Create search methods

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { almost, notQuite } = require("./src/almost");
 const { equal, different } = require("./src/equal");
+const { isMatch, search, searchNot } = require("./src/search");
 
 // They get implemented as "primitive" String methods
 String.prototype.equal = equal;
 String.prototype.diff = different;
 String.prototype.almost = almost;
 String.prototype.notQuite = notQuite;
+String.prototype.isMatch = isMatch;
 
 exports = {
     equal,
     different,
     almost,
-    notQuite
+    notQuite,
+    isMatch,
+    search,
+    searchNot,
 };
---
+++ src/search.js
@@ -0,0 +1,60 @@
+/**
+ * Determines if two values are a match or not
+ * 
+ * @param {String} original The original string to compare
+ * @param {String} compare The second string to be compared
+ * 
+ * @returns {Boolean} Are they a match?
+ */
+const isMatch = (original, compare) => almost(original, compare) || equal(original, compare);
+
+/**
+ * Search for a value in a string array
+ * 
+ * @param {Array} dataset The array containing the strings
+ * @param {String} original The original string to compare
+ * @param {Object} options The configuration options
+ * 
+ * @returns {Array} The occurences, if empty, it wasn't found
+ */
+const search = (dataset, original, options = {}) => {
+    // TODO: implement the possibility to find number of ocurrecences? return value
+    // and possibility to find if it's just there, not the number of times
+
+    let occurrences = [];
+
+    const datasetLen = dataset.length
+    for (let datasetIndex = 0; datasetIndex < datasetLen; datasetIndex++) {
+        const compare = dataset[datasetIndex];
+
+        // If it's a match, add to the occurrences
+        if (isMatch(original, compare)) occurrences.push(compare);
+    }
+
+    return occurrences;
+};
+
+/**
+ * Search for everything but the value in a string array
+ * 
+ * @param {Array} dataset The array containing the strings
+ * @param {String} original The original string to compare
+ * @param {Object} options The configuration options
+ * 
+ * @returns {Array} The occurences, if empty, it wasn't found
+ */
+const searchNot = (dataset, original, options = {}) => {
+    let occurrences = [];
+
+    const datasetLen = dataset.length
+    for (let datasetIndex = 0; datasetIndex < datasetLen; datasetIndex++) {
+        const compare = dataset[datasetIndex];
+
+        // If it's a match, add to the occurrences
+        if (!isMatch(original, compare)) occurrences.push(compare);
+    }
+
+    return occurrences;
+}
+
+exports = { isMatch: isMatch, search: search, searchNot: searchNot };
