ebb49d4432de4f83229a298e8918d4fe154b33a2
pepe_fabra_valverde <jofaval@outlook.com>
2021-12-04 22:55:21 +0100

    Refactor module export

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const { almost , notQuite          } = require("./src/almost");
-const { equal  , different         } = require("./src/equal");
+const { almost, notQuite } = require("./src/almost");
+const { equal, different } = require("./src/equal");
 const { isMatch, search, searchNot } = require("./src/search");
-const { contains                   } = require("./src/contains");
+const { contains } = require("./src/contains");
 
 // They get implemented as "primitive" String methods in their own files
 
-exports.equal     = equal;
-exports.different = different;
-exports.almost    = almost;
-exports.notQuite  = notQuite;
-exports.isMatch   = isMatch;
-exports.search    = search;
-exports.searchNot = searchNot;
-exports.contains  = contains;
+module.exports = {
+    equal,
+    different,
+    almost,
+    notQuite,
+    isMatch,
+    search,
+    searchNot,
+    contains,
+}
